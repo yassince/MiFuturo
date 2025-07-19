8e1c0349ff53d823fb1507dc11b213341177c779
ycharrouf <ycha0707@g.educaand.es>
2025-07-19 15:00:40 +0200

    refactor TransferenciaModel

--- Models/TransferenciaModel.js
+++ Models/TransferenciaModel.js
@@ -3,10 +3,10 @@ import { pool } from '../MySQL/conexion.js';
 export class TransferenciaModel {
     //Metodo para la apertura de la cuenta
     static async apreturaCuenta(origen, tipo, monto, fecha_hora, descripcion) {
-        const sql = 'INSERT INTO transacciones (cuenta_origen_id, tipo_transaccion, monto, fecha_hora ,descripcion) VALUES(?, ?, ?, ?, ?);'
+        const sql = 'INSERT INTO transacciones (cuenta_origen_id, tipo_transaccion, monto, fecha_hora ,descripcion) VALUES($1, $2, $3, $4, $5);'
 
         try {
-            await pool.execute(sql, [
+            await pool.query(sql, [
                 origen,
                 tipo,
                 monto,
@@ -22,10 +22,10 @@ export class TransferenciaModel {
     //Metodo para que el usuario realice un retiro
     static async ingreso(destino, saldo, fecha_hora, descripcion) {
 
-        const sql = 'INSERT INTO transacciones (cuenta_destino_id, tipo_transaccion, monto, fecha_hora ,descripcion) VALUES(?, ?, ?, ?, ?);'
+        const sql = 'INSERT INTO transacciones (cuenta_destino_id, tipo_transaccion, monto, fecha_hora ,descripcion) VALUES ($1, $2, $3, $4, $5);'
 
         try {
-            await pool.execute(sql, [
+            await pool.query(sql, [
                 destino,
                 'Ingreso',
                 saldo,
@@ -40,10 +40,10 @@ export class TransferenciaModel {
 
     //Metodo para que usuario haga un retiro
     static async retiro(origen, monto, fecha_hora, descripcion) {
-        const sql = 'INSERT INTO transacciones (cuenta_origen_id,tipo_transaccion,monto,fecha_hora,descripcion) VALUES(?, ?, ?, ?, ? );'
+        const sql = 'INSERT INTO transacciones (cuenta_origen_id,tipo_transaccion,monto,fecha_hora,descripcion) VALUES($1, $2, $3, $4, $5);'
 
         try {
-            await pool.execute(sql, [
+            await pool.query(sql, [
                 origen,
                 'Retiro',
                 monto,
@@ -58,10 +58,10 @@ export class TransferenciaModel {
 
     //Metodo para la transferencia al completo
     static async transferencia(origen, destino, tipo, monto, fecha_hora, descripcion) {
-        const sql = 'INSERT INTO transacciones (cuenta_origen_id, cuenta_destino_id, tipo_transaccion, monto, fecha_hora ,descripcion) VALUES(?, ?, ?, ?, ?, ?);'
+        const sql = 'INSERT INTO transacciones (cuenta_origen_id, cuenta_destino_id, tipo_transaccion, monto, fecha_hora ,descripcion) VALUES($1, $2, $3, $4, $5, $6);'
 
         try {
-            await pool.execute(sql, [
+            await pool.query(sql, [
                 origen,
                 destino,
                 tipo,
@@ -77,12 +77,12 @@ export class TransferenciaModel {
 
     //metodo para obtener todos los movimientos en los que esta involucrado el usuario
     static async getmovimientos(id) {
-        const sql = "SELECT *, CASE WHEN cuenta_origen_id = ? THEN 'salida' WHEN cuenta_destino_id = ? THEN 'entrada' ELSE 'desconocido' END AS direccion FROM transacciones WHERE cuenta_origen_id = ? OR cuenta_destino_id = ? ORDER BY fecha_hora DESC;"
+        const sql = "SELECT *, CASE WHEN cuenta_origen_id = $1 THEN 'salida' WHEN cuenta_destino_id = $2 THEN 'entrada' ELSE 'desconocido' END AS direccion FROM transacciones WHERE cuenta_origen_id = $3 OR cuenta_destino_id = $4 ORDER BY fecha_hora DESC;"
 
         try {
-            const resutl = await pool.execute(sql, [id, id, id, id])
+            const resutl = await pool.query(sql, [id, id, id, id])
 
-            return resutl[0]
+            return resutl.rows
         } catch (error) {
             throw new Error('Error al insertar transacción: ', error)
         }
